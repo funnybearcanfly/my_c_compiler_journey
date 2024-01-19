55c1880c85852e30f682951c954106a2d0d806a2
U-DESKTOP-55DIDII\Administrator <yixiong.tan@foxmail.com>
2024-01-19 23:25:13 +0800

    cmake working

---
+++ src/test1/example.c
@@ -0,0 +1,17 @@
+// C program to demonstrate 
+// auto keyword
+#include <stdio.h>
+#include "head1.h"
+#include "head2.h"
+
+int printvalue()
+{
+  printf("%d", a);
+}
+ 
+// Driver code
+int main() 
+{
+  printvalue();
+  return 0;
+}
